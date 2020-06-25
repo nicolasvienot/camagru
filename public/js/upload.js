85bf32918f6725ef6bceac29991cefe7be0cf9f2
nicolasvienot <nvienot@student.42.fr>
2020-06-26 01:17:50 +0200

    oops

--- public/js/upload.js
+++ public/js/upload.js
@@ -174,6 +174,7 @@ function share() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+	      var res = JSON.parse(this.responseText);
         if (res.result === 1) {
           var column = document.createElement("div");
           var card = document.createElement("div");
